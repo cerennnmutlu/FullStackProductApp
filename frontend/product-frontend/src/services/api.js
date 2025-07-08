ad4083dce8b8adb5d836026f043459ba77a26a43
Ceren Mutlu <cerenmutlu@MacBookPro.home>
2025-07-08 12:21:12 +0300

    fix:heroku login

--- frontend/product-frontend/src/services/api.js
+++ frontend/product-frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5261/api'; //  Not HTTPS!
-
+const API_BASE = process.env.REACT_APP_API_URL;
 
 export const fetchProducts = async () => {
-  const response = await axios.get(`${API_BASE}/products`);
+  const response = await axios.get(`${API_BASE}/api/products`);
   return response.data;
 };
